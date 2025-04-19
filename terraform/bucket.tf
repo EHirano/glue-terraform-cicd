166aaccf0a6a25178d86c1fd7bb7f92dad75e660
˜Hirano˜ <eduardoloverahirano@gmail.com>
2025-04-19 10:58:46 -0300

    fix bucket encryption

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,32 +1,35 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = "terraform-project-glue-cicd"
-}
-
-resource "aws_s3_bucket" "artifacts" {
-  bucket = var.artifacts_bucket_name
-
-  tags = {
-    Name        = var.artifacts_bucket_name
-    Environment = var.environment
-    Project     = "GlueETL"
+  resource "aws_s3_bucket" "bucket" {
+    bucket = "terraform-project-glue-cicd"
   }
-}
 
-resource "aws_s3_bucket_versioning" "artifacts" {
-  bucket = aws_s3_bucket.artifacts.id
+  resource "aws_s3_bucket" "artifacts" {
+    bucket = var.artifacts_bucket_name
 
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "artifacts" {
-  bucket = aws_s3_bucket.artifacts.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+    tags = {
+      Name        = var.artifacts_bucket_name
+      Environment = var.environment
+      Project     = "GlueETL"
     }
   }
-}
+
+  resource "aws_s3_bucket_versioning" "artifacts" {
+    bucket = aws_s3_bucket.artifacts.id
+
+    versioning_configuration {
+      status = "Enabled"
+    }
+  }
+
+  resource "aws_s3_bucket_server_side_encryption_configuration" "artifacts" {
+    bucket = aws_s3_bucket.artifacts.id
+
+    rule {
+      bucket_key_enabled = true
+
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
+
 
