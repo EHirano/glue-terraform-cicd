614841b51b83704c157d4e28f1e8e6cdd3269569
˜Hirano˜ <eduardoloverahirano@gmail.com>
2025-04-19 11:23:20 -0300

    delete unnecessary bucket

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,35 +1,31 @@
-  resource "aws_s3_bucket" "bucket" {
-    bucket = "terraform-project-glue-cicd"
+resource "aws_s3_bucket" "artifacts" {
+  bucket = var.artifacts_bucket_name
+
+  tags = {
+    Name        = var.artifacts_bucket_name
+    Environment = var.environment
+    Project     = "GlueETL"
   }
+}
 
-  resource "aws_s3_bucket" "artifacts" {
-    bucket = var.artifacts_bucket_name
+resource "aws_s3_bucket_versioning" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
 
-    tags = {
-      Name        = var.artifacts_bucket_name
-      Environment = var.environment
-      Project     = "GlueETL"
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  rule {
+    bucket_key_enabled = true
+
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
     }
   }
-
-  resource "aws_s3_bucket_versioning" "artifacts" {
-    bucket = aws_s3_bucket.artifacts.id
-
-    versioning_configuration {
-      status = "Enabled"
-    }
-  }
-
-  resource "aws_s3_bucket_server_side_encryption_configuration" "artifacts" {
-    bucket = aws_s3_bucket.artifacts.id
-
-    rule {
-      bucket_key_enabled = true
-
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
-      }
-    }
-  }
+}
 
 
